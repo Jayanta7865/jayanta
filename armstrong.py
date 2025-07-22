9d3a104b45c0c9273e3232e4bdf1bba1d89915c3
Jayanta7865 <jsamanta2003@gmail.com>
2025-07-22 20:34:01 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,16 @@
+# Check if a number is Armstrong or not
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+n = len(str(num))  # Count the digits
+
+while temp > 0:
+    digit = temp % 10       # Get last digit
+    sum += digit ** n       # Raise it to power of n and add to sum
+    temp //= 10             # Remove last digit
+
+if num == sum:
+    print(num, "is an Armstrong number.")
+else:
+    print(num, "is not an Armstrong number.")
 
